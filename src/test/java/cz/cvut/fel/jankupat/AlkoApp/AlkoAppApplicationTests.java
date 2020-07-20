a47fb55a7f466ae3cc6aeca9e433496b589546df
Patrik Jankuv <jankupat@fel.cvut.cz>
2020-07-20 23:01:55 +0200

    Initial commit

---
+++ src/test/java/cz/cvut/fel/jankupat/AlkoApp/AlkoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.jankupat.AlkoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
