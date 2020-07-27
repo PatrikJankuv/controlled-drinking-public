8bc834988f74b0838904a4e24bfa0bf1df2f66fc
Patrik Jankuv <64837148+patrik-jankuv@users.noreply.github.com>
2020-07-27 18:09:44 +0200

    add temp

---
+++ src/main/java/cz/cvut/fel/jankupat/AlkoApp/AlkoAppApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.jankupat.AlkoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkoAppApplication.class, args);
+	}
+
+}
