f000971293d1b073d8af2d88b1488920cd5645c1
Patrik Jankuv <64837148+patrik-jankuv@users.noreply.github.com>
2020-08-03 17:03:25 +0200

    drinkItem - model

---
+++ src/main/java/cz/cvut/fel/jankupat/AlkoApp/model/DrinkItem.java
@@ -0,0 +1,29 @@
+package cz.cvut.fel.jankupat.AlkoApp.model;
+
+import javax.persistence.Entity;
+
+/**
+ * @author Patrik Jankuv
+ * @created 8/3/2020
+ */
+@Entity
+public class DrinkItem extends BaseEntity{
+
+    private AlkoUser alkoUser;
+
+    private Day day;
+
+    private String name;
+
+    private DrinkTypeEnum drinkType;
+
+    private int price;
+
+    private int amount = 0;
+
+    private int alcohol;
+
+    private String description;
+
+    private Boolean planned;
+}
---
+++ src/main/java/cz/cvut/fel/jankupat/AlkoApp/model/DrinkTypeEnum.java
@@ -0,0 +1,14 @@
+package cz.cvut.fel.jankupat.AlkoApp.model;
+
+/**
+ * @author Patrik Jankuv
+ * @created 8/3/2020
+ */
+public enum DrinkTypeEnum {
+    BEER,
+    WINE,
+    SPIRIT,
+    NON_ALCOHOL,
+    FOOD,
+    ANOTHER
+}
