d91dd5f33bfe3ce77e8526fd42cb5765bf2e5e47
Patrik Jankuv <64837148+patrik-jankuv@users.noreply.github.com>
2020-09-30 14:42:20 +0200

    feelings enum change

--- src/main/java/cz/cvut/fel/jankupat/AlkoApp/model/enums/FeelingsEnum.java
+++ src/main/java/cz/cvut/fel/jankupat/AlkoApp/model/enums/FeelingsEnum.java
@@ -5,13 +5,5 @@ package cz.cvut.fel.jankupat.AlkoApp.model.enums;
  * @created 8/3/2020
  */
 public enum FeelingsEnum {
-    GOOD,
-    GREAT,
-    SELF_CONFIDENT,
-    SAD,
-    ANGRY,
-    SHY,
-    DISGUSTED,
-    SURPRISED,
-    TERRIBLE
+    ANGER, DISGUSTED, EXPECTATION, FEAR, FRUSTRATION, JOY, SAD, SHY, SURPRISED
 }
