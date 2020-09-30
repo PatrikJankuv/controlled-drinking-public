9ac202b982d9b6c7c3ec23b340bea411c84bcc39
Patrik Jankuv <64837148+patrik-jankuv@users.noreply.github.com>
2020-09-30 13:20:10 +0200

    dayController create and add reflection

--- src/main/java/cz/cvut/fel/jankupat/AlkoApp/rest/DayController.java
+++ src/main/java/cz/cvut/fel/jankupat/AlkoApp/rest/DayController.java
@@ -1,10 +1,18 @@
 package cz.cvut.fel.jankupat.AlkoApp.rest;
 
 import cz.cvut.fel.jankupat.AlkoApp.dao.DayDao;
+import cz.cvut.fel.jankupat.AlkoApp.exception.NotFoundException;
 import cz.cvut.fel.jankupat.AlkoApp.model.Day;
+import cz.cvut.fel.jankupat.AlkoApp.model.Reflection;
+import cz.cvut.fel.jankupat.AlkoApp.rest.util.RestUtils;
 import cz.cvut.fel.jankupat.AlkoApp.service.DayService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cz.cvut.fel.jankupat.AlkoApp.service.ReflectionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Patrik Jankuv
@@ -14,5 +22,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/day")
 public class DayController extends BaseController<DayService, Day, DayDao> {
-    public DayController(DayService service){ super(service);}
+    private final ReflectionService reflectionService;
+
+    @Autowired
+    public DayController(DayService service, ReflectionService reflectionService){ super(service);
+        this.reflectionService = reflectionService;
+    }
+
+
+    /**
+     *
+     * @param id
+     * @param reflection
+     * @return
+     */
+    @PostMapping(value = "{id}/reflection", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createDayAndAddToProfileDays(@PathVariable Integer id, @RequestBody Reflection reflection){
+        Day day = service.find(id);
+
+        if (day == null) {
+            throw NotFoundException.create(this.getClass().getSimpleName(), id);
+        }
+
+        reflectionService.persist(reflection);
+        day.setReflection(reflection);
+        service.update(day);
+
+        LOG.debug("Updated entity {}.", day);
+        final HttpHeaders headers = RestUtils.createLocationHeaderFromCurrentUri("/{id}", reflection.getId());
+        return new ResponseEntity<>(headers, HttpStatus.ACCEPTED);
+    }
 }
