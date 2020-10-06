4c7b3b9bb5fc0e4170b5790fbaabe03eb4161868
Patrik Jankuv <64837148+patrik-jankuv@users.noreply.github.com>
2020-10-06 15:28:47 +0200

    update profile override

--- src/main/java/cz/cvut/fel/jankupat/AlkoApp/service/ProfileService.java
+++ src/main/java/cz/cvut/fel/jankupat/AlkoApp/service/ProfileService.java
@@ -2,6 +2,7 @@ package cz.cvut.fel.jankupat.AlkoApp.service;
 
 import cz.cvut.fel.jankupat.AlkoApp.dao.ProfileDao;
 import cz.cvut.fel.jankupat.AlkoApp.model.Profile;
+import cz.cvut.fel.jankupat.AlkoApp.model.enums.AchievementEnum;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,5 +14,16 @@ public class ProfileService extends BaseService<Profile, ProfileDao> {
 
     public ProfileService(ProfileDao dao){
         super(dao);
+    }
+
+    /**
+     * Check if Profile contains achievement
+     *
+     * @param profile Profile
+     * @param achievementEnum name of achievement
+     * @return true if contains, false if not
+     */
+    public boolean containsAchievement(Profile profile, AchievementEnum achievementEnum){
+        return profile.getAchievements().stream().anyMatch(p -> p.getName().equals(achievementEnum));
     }
 }
