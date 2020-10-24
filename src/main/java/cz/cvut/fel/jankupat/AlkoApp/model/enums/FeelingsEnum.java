65ed535aca5d9dfda8a43ad23d4979383b96fe11
Patrik Jankuv <64837148+patrik-jankuv@users.noreply.github.com>
2020-10-24 16:26:59 +0200

    add feelings to feelingsenum

--- src/main/java/cz/cvut/fel/jankupat/AlkoApp/model/enums/FeelingsEnum.java
+++ src/main/java/cz/cvut/fel/jankupat/AlkoApp/model/enums/FeelingsEnum.java
@@ -5,5 +5,5 @@ package cz.cvut.fel.jankupat.AlkoApp.model.enums;
  * @created 8/3/2020
  */
 public enum FeelingsEnum {
-    ANGER, DISGUSTED, EXPECTATION, FEAR, FRUSTRATION, JOY, SAD, SHY, SURPRISED
+    ANGER, DISGUSTED, EXPECTATION, FEAR, FRUSTRATION, JOY, SAD, SHY, SURPRISED, GOOD, GREAT, CONFIDENT, ANGRY, TERRIBLE
 }
